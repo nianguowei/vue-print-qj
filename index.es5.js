9f8b530fe7b486e2b8f0b2a03e9aa7dd554a1bd2
nianguowei <niangw@hotmail.com>
2019-02-15 11:31:29 +0800

    init

--- index.es5.js
+++ index.es5.js
@@ -1,6 +1,17 @@
-import Print from './lib/print.js';
-Print.install = function (Vue) {
-  Vue.directive('print', Print);
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _print = require('./lib/print.js');
+
+var _print2 = _interopRequireDefault(_print);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_print2.default.install = function (Vue) {
+  Vue.directive('print', _print2.default);
 };
 
-export default Print;
+exports.default = _print2.default;
